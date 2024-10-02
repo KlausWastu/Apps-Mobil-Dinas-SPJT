f5810f6b2049ab97ebd0aae1370307a15446ff3c
Klaus Rajendra Wastu <wastuklaus28@gmail.com>
2024-10-02 15:57:01 +0700

    Update controller untuk edit user

--- routes/handler/user/controller.js
+++ routes/handler/user/controller.js
@@ -1,3 +1,4 @@
+const { Op } = require("sequelize");
 const { User } = require("../../../models");
 const bcrypt = require("bcrypt");
 const judul = "User";
@@ -92,8 +93,6 @@ module.exports = {
   actionEdit: async (req, res) => {
     try {
       const user = await User.findByPk(req.params.id);
-      const pass = user.password;
-      console.log(pass);
 
       const password = await bcrypt.hash(req.body.password, 12);
       let data = {
@@ -102,24 +101,28 @@ module.exports = {
         password: password,
         role: req.body.role,
       };
-      const emailAlreadyExist = await User.findOne({
-        where: {
-          email: data.email,
-        },
-      });
-      if (emailAlreadyExist) {
-        req.flash(
-          "alertMessage",
-          `Email ${emailAlreadyExist.email} Sudah Ada, Silahkan Masukan Email Lain`
-        );
-        req.flash("alertStatus", "danger");
-        res.redirect("/users");
-      } else {
-        await user.update(data);
-        req.flash("alertMessage", `User ${user.name} baru ditambahkan`);
-        req.flash("alertStatus", "success");
-        res.redirect("/users");
+
+      if (user.email !== data.email) {
+        const emailAlreadyExist = await User.findOne({
+          where: {
+            email: data.email,
+            id: { [Op.ne]: req.params.id },
+          },
+        });
+
+        if (emailAlreadyExist) {
+          req.flash(
+            "alertMessage",
+            `Email ${emailAlreadyExist.email} Sudah Ada, Silahkan Masukan Email Lain`
+          );
+          req.flash("alertStatus", "danger");
+          res.redirect("/users");
+        }
       }
+      await user.update(data);
+      req.flash("alertMessage", `User ${user.name} baru ditambahkan`);
+      req.flash("alertStatus", "success");
+      res.redirect("/users");
     } catch (err) {
       req.flash("alertMessage", `${err.message}`);
       req.flash("alertStatus", "danger");

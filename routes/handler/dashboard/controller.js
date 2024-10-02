92c9c62237d0b0c4a29c39b04ccb7e93b0d981d9
Klaus Rajendra Wastu <wastuklaus28@gmail.com>
2024-10-02 15:59:11 +0700

    Create Route, Controller and View for Logs

---
+++ routes/handler/dashboard/controller.js
@@ -0,0 +1,20 @@
+module.exports = {
+  index: async (req, res) => {
+    try {
+      const alertMessage = req.flash("alertMessage");
+      const alertStatus = req.flash("alertStatus");
+      const alert = { message: alertMessage, status: alertStatus };
+
+      res.render("pages/dashboard/view_dashboard", {
+        title: "Dashboard",
+        alert,
+        name: req.session.user.name,
+        role: req.session.user.role,
+      });
+    } catch (err) {
+      req.flash("alertMessage", `${err.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/sign-in");
+    }
+  },
+};
